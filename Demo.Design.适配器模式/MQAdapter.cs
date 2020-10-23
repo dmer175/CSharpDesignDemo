478533936b30e30e012c15fe42446be716106bae
dmer175 <1751009571@qq.com>
2020-10-23 17:52:13 +0800

    初始化 c#设计模式案例

--- Demo.Design.适配器模式/MQAdapter.cs
+++ Demo.Design.适配器模式/MQAdapter.cs
@@ -7,18 +7,18 @@ namespace Demo.Design.适配器模式
 {
     public class MQAdapter
     {
-        public static RebateInfo filter(string strJson,Dictionary<string,string> link)
-        {
-            return filter(JsonConvert.DeserializeObject(strJson), link);
-        }
+        //public static RebateInfo filter(string strJson,Dictionary<string,string> link)
+        //{
+        //    return filter(JsonConvert.DeserializeObject(strJson), link);
+        //}
 
-        private static RebateInfo filter(object obj, Dictionary<string, string> link)
-        {
-            RebateInfo rebateInfo = new RebateInfo();
-            foreach (var item in link)
-            {
-                object va = obj.GetType(item.Value);
-            }
-        }
+        //private static RebateInfo filter(object obj, Dictionary<string, string> link)
+        //{
+        //    RebateInfo rebateInfo = new RebateInfo();
+        //    foreach (var item in link)
+        //    {
+        //        object va = obj.GetType(item.Value);
+        //    }
+        //}
     }
 }
